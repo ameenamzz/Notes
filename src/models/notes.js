957e8f576f54f087350588785a19e9380b416f31
ameeenamzz <shoukethameen@gmail.com>
2025-08-28 12:11:22 +0530

    created notes Schema

---
+++ src/models/notes.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { trim } = require("validator");
+
+const notesSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      maxlength: 30,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    tag: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Notes = mongoose.model("Notes",notesSchema);
+module.exports = Notes
