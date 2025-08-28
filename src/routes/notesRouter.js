b683c9795908a3cb0e339027a0057e018f5be384
ameeenamzz <shoukethameen@gmail.com>
2025-08-28 14:17:18 +0530

    created notes/all api

--- src/routes/notesRouter.js
+++ src/routes/notesRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const Notes = require("../models/notes");
+const User = require("../models/user");
 const notesRouter = express.Router();
 
 notesRouter.post("/note/create", userAuth, async (req, res) => {
@@ -20,13 +21,39 @@ notesRouter.post("/note/create", userAuth, async (req, res) => {
     });
 
     await note.save();
-    res.json("Note Added Successfully!");
-  } catch (error) {
     res.status(201).json({
       success: true,
       message: "Note Added Successfully!",
       note,
     });
+  } catch (error) {
+    res.status(400).send("ERROR :" + error.message);
+  }
+});
+
+notesRouter.get("/notes/all", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+    const userId = user._id;
+    const notes = await Notes.find({
+      user: userId,
+    });
+
+    if (notes.length === 0) {
+      return res.status(200).json({
+        success: true,
+        message: "No notes to show",
+        notes: [],
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Your Notes!",
+      notes,
+    });
+  } catch (error) {
+    res.status(400).send("ERROR :" + error.message);
   }
 });
 
