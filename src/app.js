8e9fcbdf9b7214c83a493d1dc9dd20dc4ecef7bc
ameeenamzz <shoukethameen@gmail.com>
2025-08-29 17:32:11 +0530

    added cors middleware

--- src/app.js
+++ src/app.js
@@ -5,7 +5,14 @@ require("dotenv").config();
 var cookieParser = require("cookie-parser");
 const profileRouter = require("./routes/profileRouter");
 const notesRouter = require("./routes/NotesRouter");
+var cors = require("cors");
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(authRouter);
